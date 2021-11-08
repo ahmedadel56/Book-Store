3c691aa306c04c3b91fa518b0bdffb4dc6e5c491
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-09 01:24:41 +0200

    Install redux and react router

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../components-style/App.css'
+import Nav from './Nav';
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Nav />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../components-style/Nav.css';
+export default function Nav() {
+    return (
+        <div>
+            <div className="panel-bg">
+                <span className="Bookstore-CMS Text-Style-5">
+                    Bookstore CMS
+                </span>
+                <span className="BOOKS Text-Style-7">
+                    BOOKS
+                </span>
+                <span className="CATEGORIES Text-Style-7">
+                    CATEGORIES
+                </span>
+                <div className="Oval">
+                <div className="Mask"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
