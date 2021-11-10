463204c08e74f85ac31e1a72545153b800b1ab6d
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-10 23:18:52 +0200

    Fix review issue

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -9,7 +9,7 @@ const REMOVE_BOOK_FAILURE = 'bookStore/books/REMOVE_BOOK_FAILURE';
 
 const initialState = [];
 
-export const getBooksList = () => (dispatch) => axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ahmdadl563/books').then(
+export const getBooksList = () => (dispatch) => axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hJJWMdshaywdzMWOohu6/books').then(
   (books) => {
     const booksList = Object.entries(books.data);
 
@@ -29,7 +29,7 @@ export const getBooksList = () => (dispatch) => axios.get('https://us-central1-b
   (err) => dispatch({ type: GET_BOOKS_FAILURE, err }),
 );
 
-export const addBook = (book) => (dispatch) => axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ahmdadl563/books', {
+export const addBook = (book) => (dispatch) => axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hJJWMdshaywdzMWOohu6/books', {
   item_id: book.id,
   title: book.title,
   category: book.category,
@@ -40,7 +40,7 @@ export const addBook = (book) => (dispatch) => axios.post('https://us-central1-b
   (err) => dispatch({ type: ADD_BOOK_FAILURE, err }),
 );
 
-export const removeBook = (id) => (dispatch) => axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ahmdadl563/books/${id}`).then(
+export const removeBook = (id) => (dispatch) => axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hJJWMdshaywdzMWOohu6/books/${id}`).then(
   () => {
     dispatch({ type: REMOVE_BOOK_SUCCESS, payload: id });
   },
