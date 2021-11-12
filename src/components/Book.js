1f0f5bc8d30d7b863d20ca4be2d80a06cc3427d5
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-12 08:34:59 +0200

    Add react-icons

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
@@ -9,12 +9,44 @@ const Book = ({ book }) => {
   const remove = () => {
     dispatch(removeBook(id));
   };
+
+  const [percentage, setPercntage] = useState(0);
+  const [chapter, setCahpter] = useState(0);
+  const [author, setAuthor] = useState('');
+
+  const random = (numb) => (Math.floor(Math.random() * numb));
+
+  const percAndChapter = () => {
+    setPercntage(random(100));
+    setCahpter(random(30));
+  };
+
+  const chooseAuthor = () => {
+    const authorsList = ['Michael Bennett', 'Aleatha Romig', 'Jerry Weaver', 'Jamie McGuire', 'Aleatha Romig', 'Ade, George', 'Confucius', 'Confucius',
+      'Jenna Busch', 'Laurie Finkelstein', 'Konni Granma', 'Laurel Anne Hill', 'Mark J. Rose', 'Barbara Morriss', 'Mike Robinson', 'Naomi Brett Rourke', 'Susan Shofer',
+      'Debra Tash', 'Robert Yehling ', 'Konni Granma', 'Laurel Anne Hill', 'Mark J. Rose', 'Barbara Morriss', 'William Faulkner', 'George Orwell', 'Charles Dickens',
+      'John Steinbeck', 'Ralph Ellison', 'Harper Lee', 'Franz Kafka'];
+
+    const uthorIndex = random(31);
+    const author = authorsList[uthorIndex];
+    setAuthor(author);
+  };
+
+  useEffect(() => {
+    percAndChapter();
+    chooseAuthor();
+  }, []);
+
   return (
     <li>
       <h3>
         {title}
         {' '}
         {category}
+        {percentage}
+        %
+        {chapter}
+        {author}
       </h3>
       <button type="button" onClick={remove}> remove</button>
     </li>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaAddressBook } from 'react-icons/fa';
 import '../components-style/Nav.css';
 
 export default function Nav() {
   return (
-    <div>
-      <div className="panel-bg">
-        <span className="Bookstore-CMS ">
-          Bookstore CMS
+    <div className="panel-bg">
+      <span className="Bookstore-CMS ">
+        Bookstore CMS
+      </span>
+      <NavLink to="/" className="NavLink" activeClassName="is-active" exact>
+        <span className="BOOKS ">
+          BOOKS
         </span>
-        <NavLink to="/" className="NavLink" activeClassName="is-active" exact>
-          <span className="BOOKS ">
-            BOOKS
-          </span>
-        </NavLink>
-        <NavLink to="/categories" className="NavLink" activeClassName="is-active">
-          <span className="CATEGORIES ">
-            CATEGORIES
-          </span>
-        </NavLink>
-      </div>
+      </NavLink>
+      <NavLink to="/categories" className="NavLink" activeClassName="is-active">
+        <span className="CATEGORIES ">
+          CATEGORIES
+        </span>
+      </NavLink>
+      <span />
+      <span>
+        <FaAddressBook />
+      </span>
     </div>
   );
 }
